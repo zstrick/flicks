cd766aabc905bb215b070b460d9c2e3b94a628b3
Zack Strickland <zack.a.strickland@gmail.com>
2017-10-25 08:26:46 -0400

    Add new/create

---
+++ spec/features/create_movie_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Creating a new movie" do
+  it "saves the movie and shows the new movie's details" do
+    visit movies_url
+
+    click_link 'Add New Movie'
+
+    expect(current_path).to eq(new_movie_path)
+
+    fill_in "Title", with: "New Movie Title"
+    fill_in "Description", with: "Superheroes saving the world from villains"
+    fill_in "Rating", with: "PG-13"
+    fill_in "Total gross", with: "75000000"
+    fill_in "Released on", with: (Time.now.year - 1).to_s
+
+    click_button 'Create Movie'
+
+    expect(current_path).to eq(movie_path(Movie.last))
+    expect(page).to have_text('New Movie Title')
+  end
+end
