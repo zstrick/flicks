9499a5d724546d85771ebbe823db599418fcae43
Zack Strickland <zack.a.strickland@gmail.com>
2017-10-24 21:27:26 -0400

    Add edit/update to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,20 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    end
 end
