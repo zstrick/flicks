6fa6111f51c330239188138a93c3e011b7bbf26f
Zack Strickland <zack.a.strickland@gmail.com>
2017-10-25 17:01:40 -0400

    Add some methods to the movie model

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -33,4 +33,11 @@ describe "Viewing the list of movies" do
     expect(page).to have_text("$318,412,101.00")
   end
 
+  it "does not show a movie that hasn't yet been released" do
+    movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+    visit movies_path
+
+    expect(page).not_to have_text(movie.title)
+  end
 end
