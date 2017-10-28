d247b1dd99a688ff0913e7c6c3b2dd33a64a0f12
Zack Strickland <zack.a.strickland@gmail.com>
2017-10-28 12:20:13 -0400

    Add average stars to movies

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+  def format_average_stars(movie)
+    if movie.average_stars.nil?
+      content_tag(:strong, "No reviews")
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1), "star")
+    end
+  end
 end
