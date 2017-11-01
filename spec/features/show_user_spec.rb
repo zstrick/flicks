cf6069c021e361cf22e8f912f3aa05978a463c28
Zack Strickland <zack.a.strickland@gmail.com>
2017-11-01 15:33:55 -0400

    Add sidebar content and dynamic title

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -11,4 +11,29 @@ describe "Viewing a user's profile page" do
     expect(page).to have_text(user.name)
     expect(page).to have_text(user.email)
   end
+
+  it "shows the user's favorites in the sidebar" do
+    user = User.create!(user_attributes)
+
+    movie = Movie.create!(movie_attributes)
+    user.favorite_movies << movie
+
+    sign_in(user)
+
+    visit user_url(user)
+
+    within("aside#sidebar") do
+      expect(page).to have_text(movie.title)
+    end
+  end
+
+  it "includes the user's name in the page title" do
+    user = User.create!(user_attributes)
+
+    sign_in(user)
+
+    visit user_url(user)
+
+    expect(page).to have_title("Flix - #{user.name}")
+  end
 end
